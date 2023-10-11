7ccb9ab293241d86e3cbad94f72d8769f7fcf978
512kbb <dieg.parraa@duocuc.cl>
2023-10-11 10:48:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace fizzbuzz;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int x = 1; x <= 255; x++) {
+            if (x % 3 == 0 && x % 5 == 0) {
+                Console.WriteLine("fizzbuzz");
+            } else if (x % 5 == 0) {
+                Console.WriteLine("buzz");
+            } else if (x % 3 == 0) {
+                Console.WriteLine("fizz");
+            } else {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
